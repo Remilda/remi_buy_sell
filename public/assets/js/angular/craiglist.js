a2ebd633a1035769ef13bc2d7a3645431c228ce7
tailoredtech <abhijeet.umbarkarcmpn2@gmail.com>
2017-10-10 16:59:10 +0530

    Including partial files from another directory

--- public/assets/js/angular/craiglist.js
+++ public/assets/js/angular/craiglist.js
@@ -1,4 +1,4 @@
-var craiglist = angular.module('craiglist', []);
+var craiglist = angular.module('craiglist', ["ui.router"]);
 craiglist.controller("FrontController", function($scope){
 	$scope.message = "01234567898";
 });
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,14 @@ router.use('/api', require('./api'));
 router.get('/', function (req,res,next) {
 	res.sendFile(view+'/index.html');
 })
+router.get('/:fname', function (req,res,next) {
+	var str = req.params.fname;
+	console.log(str);
+	console.log(str.indexOf(".html"));
+	if(str.indexOf(".html") > -1){
+		res.sendFile(view+'/partials/'+req.params.fname);
+	}else{
+		res.json({"status":404, "message":req.params.fname+" file not found or invalid extensions"});
+	}
+})
 module.exports = router;
