49b4d62bd0f1be6fde8f97eab7b0a4eeab293e41
tailoredtech <abhijeet.umbarkarcmpn2@gmail.com>
2017-10-11 10:38:58 +0530

    Adding angular ui bootstrap

--- public/assets/js/angular/controllers/HeaderController.js
+++ public/assets/js/angular/controllers/HeaderController.js
@@ -1,9 +1,26 @@
-craiglist.controller("HeaderController", ['$scope', function($scope){
+craiglist.controller("HeaderController", ['$scope', '$modal',function($scope,$modal){
 	$scope.message = "9920778180";
-	$scope.openSignin = function(){
+	$scope.openSignin = function(ref){
 		alert("Welcome to craiglist, please login to continue!!!");
+		loginModalInstance = $modal.open({
+			templateUrl: '../../../../views/partials/signinmodal.html',
+			controller: 'LoginModalController',
+			resolve: {
+				source: function (){
+					return ref;
+					//return "dashboard" ;
+				}
+			}
+		});
+		if (typeof signupModalInstance != "undefined") {
+			signupModalInstance.dismiss('cancel');
+		}
 	}
 	$scope.openSignUp = function(){
 		alert("Welcome to craiglist, please register to continue!!!");
 	}
+}]);
+
+craiglist.controller("LoginModalController", ['$scope', '$rootScope', function($scope,$rootScope){
+	
 }]);
--- public/assets/js/angular/craiglist.js
+++ public/assets/js/angular/craiglist.js
@@ -1,4 +1,1 @@
-var craiglist = angular.module('craiglist', ["ui.router"]);
-craiglist.controller("FrontController", function($scope){
-	$scope.message = "01234567898";
-});
+var craiglist = angular.module('craiglist', ["ui.router", "ui.bootstrap"]);
